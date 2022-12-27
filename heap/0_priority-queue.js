cbfcc03081a787d4bdb6e6b088dad64311c44fcf
Sachin Sapkal <vibhasachin@outlook.com>
2022-12-27 22:16:01 +0530

    added enqueue of priority queue

---
+++ heap/0_priority-queue.js
@@ -0,0 +1,32 @@
+class Node {
+    constructor(val, priority){
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue{
+    constructor(){
+        this.values = [];
+    }
+
+    enqueue(val, priority){
+        let newNode = new Node(val, priority);
+        this.values.push(newNode);
+        this.bubbleUp();
+    }
+
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element.priority >= parent.priority) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+}
