eeb99deadd3b7663234788eeb28390b15ac3bd25
Sachin Sapkal <vibhasachin@outlook.com>
2023-01-14 00:25:11 +0530

    added simple graph implementation

---
+++ graphs/0_implement-graph.js
@@ -0,0 +1,36 @@
+class Graph{
+    constructor(){
+        this.adjacencyList = {}
+    }
+
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex] = []
+        }
+    }
+
+    addEdge(v1, v2){
+        if(this.adjacencyList[v1] && this.adjacencyList[v2]){
+            this.adjacencyList[v1].push(v2);
+            this.adjacencyList[v2].push(v1);            
+        }
+    }
+
+    removeEdge(v1, v2){
+        if(!this.adjacencyList[v1] && this.adjacencyList[v2]) return
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
+    }
+
+    removeVertex(v){
+        if(!this.adjacencyList[v]) return;
+
+        const targets = this.adjacencyList[v];
+
+        for(const v2 of targets){
+            this.removeEdge(v, v2)
+        }
+
+        delete this.adjacencyList[v]
+    }
+}
