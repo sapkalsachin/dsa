d0b409ac8ce9627c34f8fc28347368e2c9338e3e
Sachin Sapkal <vibhasachin@outlook.com>
2023-01-18 21:03:57 +0530

    added lognest consecutive sequence

---
+++ array_and_hashing/6_longest-consecutive-sequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+
+    const set = new Set(nums);
+
+
+    let maxCount = 0;
+
+    for(const num of nums){
+        if(set.has(num-1)){
+            continue;            
+        }    
+        var [currentNum, count] = [num, 0]
+        const hasNext = (n)=> set.has(n)
+        while(hasNext(currentNum)){
+            ++currentNum;
+            ++count;        
+        }
+
+        maxCount = Math.max(maxCount, count);
+    }
+
+    return maxCount;
+};
