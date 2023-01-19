2640ed26cd1b6dbb1aa02b239d590afa81020f84
Sachin Sapkal <vibhasachin@outlook.com>
2023-01-19 21:23:43 +0530

    added question description

--- array_and_hashing/6_longest-consecutive-sequence.js
+++ array_and_hashing/6_longest-consecutive-sequence.js
@@ -1,4 +1,8 @@
 /**
+ * 
+ * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ * You must write an algorithm that runs in O(n) time.
+ * 
  * @param {number[]} nums
  * @return {number}
  */
