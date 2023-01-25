3641c8152fed0482dcce724b28f91a57b46777be
Sachin Sapkal <vibhasachin@outlook.com>
2023-01-25 16:16:37 +0530

    added sliding window problem

--- array_and_hashing/6_longest-consecutive-sequence.js
+++ array_and_hashing/6_longest-consecutive-sequence.js
@@ -1,5 +1,6 @@
 /**
- * 
+ * LEETCODE
+ * Level: medium
  * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
  * You must write an algorithm that runs in O(n) time.
  * 
