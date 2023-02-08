8c83bbc5a5f711aef5587dad879e2aaf936e0593
Sachin Sapkal <vibhasachin@outlook.com>
2023-02-08 22:40:10 +0530

    updated continer with most water problem

--- two-pointers/3_container-with-most-water.js
+++ two-pointers/3_container-with-most-water.js
@@ -1,13 +1,31 @@
 /**
- * @param {number[]} height
- * @return {number}
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
  */
-var maxArea = function(height) {
+var maxArea = function (height) {
     let maxArea = 0;
     let left = 0;
     let right = height.length - 1;
 
-    while(left < right){
+    while (left < right) {
         const h = Math.min(height[left], height[right]);
         const w = right - left;
 
@@ -15,13 +33,13 @@ var maxArea = function(height) {
 
         maxArea = Math.max(maxArea, currentArea);
 
-        if(height[left] === height[right]){
-            left ++;
-            right --;
-        }else if(height[left] < height[right]){
-            left ++;
-        }else{
-            right --;
+        if (height[left] === height[right]) {
+            left++;
+            right--;
+        } else if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
         }
     }
     return maxArea;
