ca9daabd3ae663eb8a971937294f3fc2577c605a
Sachin Sapkal <vibhasachin@outlook.com>
2023-02-14 23:04:12 +0530

    added pascal trianelg problem

---
+++ array_and_hashing/12_pascal-triangle.js
@@ -0,0 +1,26 @@
+var generate = function(num) {
+    
+    const outerArray = [];
+    // adding first two rows of pascals triangle
+    if (num >= 2) {
+      outerArray.push([1]);
+      outerArray.push([1, 1]);
+    } else {
+      outerArray.push([1]);
+    }
+  
+    // will  only run if we had number greater than 2
+    if (num > 2) {
+      for (let i = 2; i < num; i++) {
+        let subArray = [];
+         subArray.push(1);
+        for (let j = 0; j < outerArray[i - 1].length - 1; j++) {
+          subArray.push(outerArray[i - 1][j] + outerArray[i - 1][j + 1]);
+        }
+        subArray.push(1);
+        outerArray.push(subArray);
+      }
+    }
+  
+    return outerArray;
+ }; 
