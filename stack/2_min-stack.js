db71862eb3ee849606906f1b1f6b41300b20ce12
Sachin Sapkal <vibhasachin@outlook.com>
2023-02-17 23:18:55 +0530

    added min-stack problem

---
+++ stack/2_min-stack.js
@@ -0,0 +1,49 @@
+class MinStack {
+    /**
+     * @constructor
+     */
+    constructor () {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push (val, { minStack } = this) {
+        this.stack.push(val);             /* Space O(N) */
+
+        const isMinEmpty = !minStack.length;
+        const hasNewMin = val <= this.top(minStack);
+        const canAddMin = isMinEmpty || hasNewMin;
+        if (canAddMin) minStack.push(val);/* Space O(N) */
+    }
+
+    /**
+     * @return {void}
+     */
+    pop ({ stack, minStack } = this) {
+        const top = stack.pop();          /* Time O(1) */
+
+        const canPopMin = top === this.getMin();
+        if (canPopMin) minStack.pop();    /* Time O(1) */
+    }
+
+    /**
+     * @param {Array}
+     * @return {number}
+     */
+    top (stack = this.stack) {
+        return stack.length
+            ? stack[stack.length - 1]     /* Time O(1) */
+            : null;
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin (minStack = this.minStack) {
+        return this.top(minStack);       /* Time O(1) */
+    }
+}
